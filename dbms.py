1bd9e7a85dbc5d64bed7f3a5f32a9d9cbdc5ea54
Sanskriti-Srivastava <73484291+Sanskriti-Srivastava@users.noreply.github.com>
2020-11-02 13:57:29 +0530

    Update dbms.py

--- dbms.py
+++ dbms.py
@@ -1,1 +1,22 @@
 print("welcome")
+a = 100
+b = 75
+if a > b:
+    print(a)
+    for count in range(6):
+       triangle()
+
+digits = [0,1,2,3,4,5,6,7,8,9]
+print(digits)
+window_size = 2
+for i in range(len(digits)-(window_size-1)):
+   print(digits[i:i+window_size])
+#tuple
+person1 = ['sanskriti',20 ,'varanasi']
+person2 = ['saniya',19,'sangamner']
+people = [person1 , person2]
+for name, age, city in people:
+            print(name)
+            print(age)
+            print(city)
+            print()
